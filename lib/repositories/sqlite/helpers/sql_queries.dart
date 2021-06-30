5cb62e4249f57212d9e892db2094e60196005bec
angelo-squadra <angelo.savioti@squadra.com.br>
2021-06-30 20:17:40 -0300

    mais campos adicionados ao form

--- lib/repositories/sqlite/helpers/sql_queries.dart
+++ lib/repositories/sqlite/helpers/sql_queries.dart
@@ -1,6 +1,6 @@
 class SqlQueries {
   static const String createEmployeeTable =
-      'CREATE TABLE $employeesTableName(id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, sector TEXT, level TEXT, outsourced INTEGER)';
+      'CREATE TABLE $employeesTableName(id INTEGER PRIMARY KEY, name TEXT, birthday TEXT, gender TEXT, sector TEXT, level TEXT, outsourced INTEGER)';
 
   static const String employeesTableName = 'employees';
 }
