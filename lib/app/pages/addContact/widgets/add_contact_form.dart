cf860234890ba0b3ec819accf99427b6cc702de5
angelo-squadra <angelo.savioti@squadra.com.br>
2021-07-07 14:25:56 -0300

    adding contact

--- lib/app/pages/addContact/widgets/add_contact_form.dart
+++ lib/app/pages/addContact/widgets/add_contact_form.dart
@@ -1,3 +1,5 @@
+import 'package:estudo_sqlite/services/sqlite_service.dart';
+import 'package:estudo_sqlite/shared/models/contact.dart';
 import 'package:flutter/material.dart';
 
 class AddContactForm extends StatefulWidget {
@@ -6,6 +8,7 @@ class AddContactForm extends StatefulWidget {
 }
 
 class _AddContactFormState extends State<AddContactForm> {
+  final SqliteService _sqliteService = SqliteService();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
@@ -71,8 +74,12 @@ class _AddContactFormState extends State<AddContactForm> {
                     ElevatedButton(
                       onPressed: () {
                         if (_formKey.currentState!.validate()) {
-                          Navigator.pop(context);
                           Navigator.pushReplacementNamed(context, '/');
+                          Contact contact = Contact(
+                            name: _nameController.text,
+                            phone: _phoneController.text,
+                          );
+                          _sqliteService.addContact(contact);
                         }
                       },
                       child: Text('Submit'),
